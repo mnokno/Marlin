cebb12c275c6c8702608afbb489ec95c0b5ce619
mnokno <prv.nowacki@gmail.com>
2022-11-18 16:44:49 +0000

    Successfully integrated google test into the project

---
+++ Google_test/test1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kubaa on 18/11/2022.
+//
+
+#include "gtest/gtest.h"
+
+TEST (SquareTest, PosZeroNeg) {
+    ASSERT_EQ (0.0, (0.0));     // success
+    ASSERT_EQ (0.0, (0.0));
+}
+
+TEST (SquareTest2, PosZeroNeg2) {
+    ASSERT_EQ (0.0, (0.0));     // success
+    ASSERT_EQ (0.0, (0.0));
+}
---
+++ Google_test/tests2.cpp
@@ -0,0 +1,3 @@
+//
+// Created by kubaa on 18/11/2022.
+//

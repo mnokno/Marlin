ba7f02599c62c3d937d223101cb5a626175c45dd
mnokno <prv.nowacki@gmail.com>
2022-11-18 13:48:04 +0000

    restarted to project due to some issues with the project structure.

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "position.h"
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    engine::Position* position = new engine::Position();
-    std::cout << position->test(2, 3) << std::endl;
-    return 0;
-}

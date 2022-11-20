0f90b074d6a1a1acda0f074987fbc6f31a619f32
mnokno <prv.nowacki@gmail.com>
2022-11-20 17:51:27 +0000

    Created skeleton for prng

---
+++ src/engine/prng.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kubaa on 20/11/2022.
+//
+
+#include "prng.h"
+
+namespace engine {
+} // engine
---
+++ src/engine/prng.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 20/11/2022.
+//
+
+#ifndef MARLIN_PRNG_H
+#define MARLIN_PRNG_H
+
+namespace engine {
+
+    class prng {
+
+    };
+
+} // engine
+
+#endif //MARLIN_PRNG_H

90e8e54097df81baefa7813ac05162e4a95dc3b4
mnokno <prv.nowacki@gmail.com>
2022-11-20 17:30:40 +0000

    Created position class skeleton

---
+++ src/engine/position.cpp
@@ -0,0 +1,9 @@
+//
+// Created by kubaa on 20/11/2022.
+//
+
+#include "position.h"
+
+namespace engine{
+
+}
---
+++ src/engine/position.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 20/11/2022.
+//
+
+#ifndef MARLIN_POSITION_H
+#define MARLIN_POSITION_H
+
+namespace engine{
+    class Position {
+
+    };
+}
+
+
+
+#endif //MARLIN_POSITION_H

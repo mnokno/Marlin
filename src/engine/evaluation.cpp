1140d291d99215c9b0229eaabe169a58543a1a47
mnokno <prv.nowacki@gmail.com>
2022-11-21 14:06:36 +0000

    created files for evaluation

---
+++ src/engine/evaluation.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kubaa on 21/11/2022.
+//
+
+#include "evaluation.h"
+
+namespace engine {
+} // engine
---
+++ src/engine/evaluation.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 21/11/2022.
+//
+
+#ifndef MARLIN_EVALUATION_H
+#define MARLIN_EVALUATION_H
+
+namespace engine {
+
+    class evaluation {
+
+    };
+
+} // engine
+
+#endif //MARLIN_EVALUATION_H

d7a7a65d716a808d1a40b9c01aa69a47dcc4a919
mnokno <prv.nowacki@gmail.com>
2022-11-21 14:10:56 +0000

    Created constants class

---
+++ src/engine/constants.h
@@ -0,0 +1,12 @@
+//
+// Created by kubaa on 21/11/2022.
+//
+
+#ifndef MARLIN_CONSTANTS_H
+#define MARLIN_CONSTANTS_H
+
+namespace engine{
+    const int infinity = 9999999;
+}
+
+#endif //MARLIN_CONSTANTS_H

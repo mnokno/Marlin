4e17da8b2384103a7b8f6895c183987ac615da06
mnokno <prv.nowacki@gmail.com>
2022-11-22 12:05:08 +0000

    Created files for transposition table

---
+++ src/engine/transposition_table.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kubaa on 22/11/2022.
+//
+
+#include "transposition_table.h"
+
+namespace engine {
+} // engine
---
+++ src/engine/transposition_table.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 22/11/2022.
+//
+
+#ifndef MARLIN_TRANSPOSITION_TABLE_H
+#define MARLIN_TRANSPOSITION_TABLE_H
+
+namespace engine {
+
+    class TranspositionTable {
+
+    };
+
+} // engine
+
+#endif //MARLIN_TRANSPOSITION_TABLE_H

efbc56f36c141b6119e932c678ddc6b8393e4146
mnokno <prv.nowacki@gmail.com>
2022-11-24 03:14:02 +0000

    Implemented more advanced static evaluation

--- src/engine/evaluation.cpp
+++ src/engine/evaluation.cpp
@@ -41,6 +41,32 @@ namespace engine {
     }
 
     int Evaluation::winingPossibilityEval(Position& position) {
-        return 0;
+        // allocates memory to score counters
+        int score[2] = {0, 0};
+
+        // finds edgesExtractionMask used to extract edges from the position
+        ulong edgesExtractionMask = 0;
+        for (int j = 0; j < 7; j++){
+            edgesExtractionMask |= PrecalculatedData::pillarMasks[position.convertFileToMove(j)];
+        }
+
+        // finds score for yellow and red
+        for (int i = 0; i < 2; i++){
+            ulong exposedOnEdge = position.getPosition(i) & edgesExtractionMask;
+            int opponent = i == 0 ? 1 : 0;
+
+            while (!BitOps::isEmpty(exposedOnEdge)){
+                int nextBitPos = BitOps::bitScanForward(exposedOnEdge);
+                for (ulong winingLine : PrecalculatedData::winingLinesMasks[nextBitPos]){
+                    if ((winingLine & ~position.getPosition(opponent)) == winingLine){
+                        score[i] += 1;
+                    }
+                }
+                exposedOnEdge ^= (ulong)1 << nextBitPos;
+            }
+        }
+
+        // return the difference between yellows and reds score
+        return score[0] - score[1];
     }
 } // engine

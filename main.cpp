3b70799266e61c4038aca5e4a6a166d1c4007a7e
mnokno <prv.nowacki@gmail.com>
2022-12-15 01:52:35 +0000

    Clued-up main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
-#include "search.h"
-#include "position.h"
-#include "constants.h"
-#include "precalculated_data.h"
-#include "zobrist_hashing.h"
-
-using namespace engine;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    PrecalculatedData::init();
-    ZobristHashing::initHashes();
-    Position position = *new Position();
-    TranspositionTable tt = *new TranspositionTable(TranspositionTable::calculateTableCapacity(5000));
-    Search search = *new Search(position, tt);
-    cout << search.findBestMoveABMT(14) << endl;
-    //cout << search.findBestMoveBaseTest(12, BaseLevel::ALPHA_BETA) << endl;
     return 0;
 }

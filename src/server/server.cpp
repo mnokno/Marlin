08257337f9e3d8095fe7efd5840885892fc104e5
mnokno <prv.nowacki@gmail.com>
2022-12-18 23:46:50 +0000

    Created an empty server class

---
+++ src/server/server.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kubaa on 18/12/2022.
+//
+
+#include "server.h"
+
+namespace hosting {
+} // hosting
---
+++ src/server/server.h
@@ -0,0 +1,16 @@
+//
+// Created by kubaa on 18/12/2022.
+//
+
+#ifndef MARLIN_SERVER_H
+#define MARLIN_SERVER_H
+
+namespace hosting {
+
+    class server {
+
+    };
+
+} // hosting
+
+#endif //MARLIN_SERVER_H

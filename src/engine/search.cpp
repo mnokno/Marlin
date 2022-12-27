737d84fc429f56c41e3bb257f5a07648c8328c25
mnokno <prv.nowacki@gmail.com>
2022-12-27 17:48:29 +0000

    Documented searchAlphaBetaTask

--- src/engine/search.cpp
+++ src/engine/search.cpp
@@ -267,10 +267,12 @@ namespace engine {
     /**
      * Starts a search and saves the result, can by used to spawn multiple threads
      *
-     * @param search Reference to initial search
      * @param lPosition Copy of the position
-     * @param id Id for which to associate the result
      * @param depth Depth of the search
+     * @param result Reference to the result
+     * @param abort Reference to the abort flag
+     * @param tt Reference to the transposition table
+     * @param id thread id
      */
     void Search::searchAlphaBetaTask(Position lPosition, int depth, int& result, bool& abort, TranspositionTable& tt, int id) {
         // ensures that correct abort flag is used

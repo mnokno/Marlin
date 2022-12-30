65f734f852a05eaaad9d5563cf4f3a378103e6f4
mnokno <prv.nowacki@gmail.com>
2022-12-30 20:47:51 +0000

    Added comments to prng.cpp

--- src/engine/prng.cpp
+++ src/engine/prng.cpp
@@ -6,15 +6,29 @@
 
 namespace engine {
 
+    /**
+     * Creates a new instance of PRNG with a predefined seed.
+     * implementation of xorshift64star Pseudo-Random Number Generator
+    */
     PRNG::PRNG() {
         this->state = (ulong)1070372;
     }
 
+    /**
+     * Creates a new instance of PRNG with a given seed.
+     * implementation of xorshift64star Pseudo-Random Number Generator
+     *
+     * @param seed The seed to use.
+    */
     PRNG::PRNG(ulong seed) {
         this->state = seed;
     }
 
-    // implementation of xorshift64star Pseudo-Random Number Generator
+    /**
+     * Returns the next random number,
+     *
+     * @return The next random number.
+     */
     ulong PRNG::nextUlong() {
         state ^= state >> (ulong)12;
         state ^= state << (ulong)25;

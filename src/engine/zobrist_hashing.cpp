70683cdda81f6aa7aa84d412160551d9e6011fb7
mnokno <prv.nowacki@gmail.com>
2022-12-30 20:55:18 +0000

    Added comments to zobrist_hashing.cpp

--- src/engine/zobrist_hashing.cpp
+++ src/engine/zobrist_hashing.cpp
@@ -9,6 +9,9 @@
 
 namespace engine {
 
+    /**
+     * Initializes the random numbers used for hashing.
+     */
     void ZobristHashing::initHashes() {
         PRNG prng = *new PRNG();
         for (int i = 0; i < 2; i++){
@@ -18,6 +21,12 @@ namespace engine {
         }
     }
 
+    /**
+     * Calculates hash for a new position that can be later updated move by move.
+     *
+     * @param position the position to calculate the hash for.
+     * @return the hash of the position.
+     */
     ulong ZobristHashing::generateHash(Position *position) {
         ulong hash = 0;
         for (int i = 0; i < 2; i++){
@@ -31,6 +40,14 @@ namespace engine {
         return hash;
     }
 
+    /**
+     * Updates hash based on the move that was made.
+     *
+     * @param hash hash to update.
+     * @param move played move.
+     * @param player player who played the move.
+     * @return updated hash.
+     */
     ulong ZobristHashing::updateHash(ulong hash, int move, Player player) {
         return hash ^ hashes[player][move];
     }
